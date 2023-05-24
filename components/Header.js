d6ca10eb9feebd31a4fa2895e5835dd8d2ece45f
occiandiaali <ocean.diaali@gmail.com>
2023-05-24 10:27:21 +0100

    Add: Socials dropdown menu, transparent header effect on scroll

--- components/Header.js
+++ components/Header.js
@@ -1,33 +1,42 @@
+import { useEffect, useState } from "react";
+import Socials from "./Socials";
+
 function Header() {
+  const [clientWindowHeight, setClientWindowHeight] = useState("");
+  const [bgTransparent, setBgTransparent] = useState(0);
+  const [padding, setPadding] = useState(20);
+
+  const handleScroll = () => {
+    setClientWindowHeight(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
+
+  useEffect(() => {
+    let bgTransparentVar = clientWindowHeight / 600;
+
+    if (bgTransparentVar < 1) {
+      let paddingVar = 30 - bgTransparentVar * 20;
+      let boxShadowVar = bgTransparentVar * 0.1;
+      setBgTransparent(bgTransparentVar);
+      setPadding(paddingVar);
+    }
+  }, [clientWindowHeight]);
+
   return (
-    <div className="flex justify-between my-8 items-center sticky top-0">
+    <div
+      style={{
+        background: `rgba(255, 255, 255, ${bgTransparent})`,
+        padding: `${padding}px 0px 10px 20px`,
+      }}
+      className="flex justify-between my-8 items-center sticky top-0"
+    >
       <h1 className="font-medium text-xl">Occian Drops 💧</h1>
       <div>
-        <a
-          href="https://twitter.com/occiandiaali"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="mr-4"
-        >
-          Twitter
-        </a>
-        <a
-          href="https://github.com/occiandiaali"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="mr-4"
-        >
-          Github
-        </a>
-
-        <a
-          href="https://linkedin.com/occiandiaali"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="mr-4"
-        >
-          LinkedIn
-        </a>
+        <Socials />
         <a
           href="https://i-am-occian.firebaseapp.com"
           target="_blank"
--- pages/[slug].js
+++ pages/[slug].js
@@ -8,7 +8,7 @@ function Post({ data, content }) {
   return (
     <div>
       <Link href="/">
-        <a className="text-blue-400 text-lg">&#x219a; back</a>
+        <a className="text-blue-400 text-lg">&#x219a; all drops</a>
       </Link>
       <h1 className="font-bold text-7xl mt-24 mb-12">{data.title}</h1>
       <time className="text-gray-500 italic">{data.date}</time>
