ba31c60470c7db11a48a72ec81a68edbed32d900
occiandiaali <ocean.diaali@gmail.com>
2023-07-26 11:50:37 +0100

    Add: new post with its image assets

--- components/PostCard.js
+++ components/PostCard.js
@@ -3,7 +3,9 @@ import Link from "next/link";
 function PostCard({ title, date, description, slug }) {
   return (
     <div className="my-4 py-4 border-b">
-      <h2 className="font-bold text-2xl my-4">{title}</h2>
+      <Link href="/[slug]" as={`/${slug}`} passHref>
+        <h2 className="font-bold text-2xl my-4 cursor-pointer">{title}</h2>
+      </Link>
       <time className="text-gray-400">{date}</time>
       <p className="mt-4 italic">{description}</p>
 
